cd04885589960faf114e99cbebca8322f41d01b1
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-06 19:40:21 -0300

    Parse data obtained from API

--- ruby_web_nasa.rb
+++ ruby_web_nasa.rb
@@ -6,15 +6,23 @@ require "json"
 
 #request method
 def request(url, api_key)
+
+    #request to NASA API
     url = URI("#{url}&api_key=#{api_key}")
     https = Net::HTTP.new(url.host, url.port);
     https.use_ssl = true
     request = Net::HTTP::Get.new(url)
     response = https.request(request)
-    puts response.read_body
+    data = response.read_body
+
+    #Parsing data 
+    data = JSON.parse(data)
+
+    #Output generation
+
+    
 end
 
-data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10","p4qrdNNDbxlw2IKbyoVbO41EFviwWAAApE6Bk9E7") 
+info = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10","p4qrdNNDbxlw2IKbyoVbO41EFviwWAAApE6Bk9E7") 
 
-print "#{data}\n"
-
+print "#{info}"
